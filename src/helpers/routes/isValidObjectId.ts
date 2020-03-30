d63797fd1b19ced729e81ae66f24bc37102975cf
Matt Massoodi <matt@puroxy.io>
2020-03-29 23:21:50 -0700

    Added Passport custom error config response

--- src/helpers/routes/isValidObjectId.ts
+++ src/helpers/routes/isValidObjectId.ts
@@ -1,7 +1,7 @@
-import { Context, Next } from 'koa';
+import { Context, Next, Middleware } from 'koa';
 import { isValidObjectId } from 'mongoose'
 
-export async function isObjectId(ctx: Context, next: Next): Promise<any> {
+export async function isObjectId(ctx: Context, next: Next): Promise<Middleware> {
   const { id } = ctx.params
   if (!isValidObjectId(id)) {
     return ctx.throw(400, { success: false, message: 'Invalid id type.' })
--- src/middlewares/passport/index.ts
+++ src/middlewares/passport/index.ts
@@ -1,12 +1,12 @@
 import '../../config/env'
-import { Middleware } from 'koa'
+import { Middleware, Context, Next } from 'koa'
 import { use, authenticate, initialize } from 'koa-passport'
 import { ExtractJwt, Strategy } from 'passport-jwt'
 import { IUser, User, IChef, Chef, IAdmin, Admin } from '../../models';
 
 export const secret: string = process.env.JWT_KEY
 
-export type AuthenticationRoles = 'user' | 'chef' | 'admin'
+export type AuthorizedRoles = 'user' | 'chef' | 'admin'
 
 const opts = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -37,8 +37,15 @@ use('admin', new Strategy(opts, async ({ id }, done) => {
   done(null, false)
 }))
 
-export function isAuthenticated(...types: AuthenticationRoles[]): Middleware {
-  return authenticate(types, { session: false, failWithError: true })
+export function isAuthenticated(...types: AuthorizedRoles[]): Middleware {
+  return (ctx: Context, next: Next) => {
+    return authenticate(types, { session: false }, async (err, user) => {
+      if (err || !user) {
+        return ctx.throw(401, { success: false, message: 'Unauthorized' })
+      }
+      await next()
+    })(ctx, next)
+  }
 }
 
 export default (): Middleware => {
